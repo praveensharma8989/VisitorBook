9da21c78149245d07299579590d83d7afe83abbe
Praveen Kumar <praveen@mobikasa.com>
2018-09-04 01:04:25 +0530

    start

---
+++ VisitorBook/Applications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisitorBook
+//
+//  Created by Praveen Sharma on 03/09/18.
+//  Copyright © 2018 Praveen Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
