e86db7c15fc93e18da36b0804e9e35e1b6bd8dd6
Praveen Sharma <praveensharma8989@gmail.com>
2018-09-04 01:36:12 +0530

    minor changes

---
+++ VisitorBook/Helper/CommanFunctions.swift
@@ -0,0 +1,14 @@
+//
+//  CommanFunctions.swift
+//  VisitorBook
+//
+//  Created by Praveen Sharma on 04/09/18.
+//  Copyright © 2018 Praveen Sharma. All rights reserved.
+//
+
+import Foundation
+
+class CommanFunction: NSObject {
+    
+    
+}
