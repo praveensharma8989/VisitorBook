17bc337b2597c7094463d6d15de37728cebda670
Praveen Sharma <praveensharma8989@gmail.com>
2018-10-05 14:58:18 +0530

    minor changes

---
+++ VisitorBook/Controllers/Resident/AboutUsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  AboutUsViewController.swift
+//  VisitorBook
+//
+//  Created by Praveen Sharma on 05/10/18.
+//  Copyright © 2018 Praveen Sharma. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: ResidentAllPageViewController {
+    @IBOutlet weak var aboutUsLabel: UILabel!
+    var aboutUs : AboutUsData? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initilize()
+        // Do any additional setup after loading the view.
+    }
+    
+    func initilize(){
+        
+        navigationController?.navigationBar.items![1].setHidesBackButton(true, animated: false)
+        setBackBarButton(buttonType: .Defauld)
+        CallAboutUs()
+        
+    }
+
+    func setData(){
+        
+        aboutUsLabel.text = (aboutUs?.aboutus)!
+        
+    }
+    
+    func CallAboutUs(){
+        
+        showLoader()
+        
+        let param : [String : Any] = [
+            "id" : (residentData?.id)!
+        ]
+        
+        PSServiceManager.CallAboutUs(param: param) { (response, status, error) -> (Void) in
+            self.dismissLoader()
+            if status{
+                let jsonData = try? JSONSerialization.data(withJSONObject: response!)
+                let jsonDecoder = JSONDecoder()
+                self.aboutUs = try? jsonDecoder.decode(AboutUsData.self, from: jsonData!)
+                self.setData()
+            }else{
+                self.showAlertMessage(titleStr: "Error", messageStr: error!)
+            }
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
